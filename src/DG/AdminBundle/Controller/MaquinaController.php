380ba8d3c85f37235118d85d66a99396f8bb13ff
guillermo <guillermo@monkey-pc01>
2016-05-17 16:27:45 -0600

    Merge origin/master
    Conflicts:
    	.gitignore
    	README.md

---
+++ src/DG/AdminBundle/Controller/MaquinaController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace DG\AdminBundle\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use DG\AdminBundle\Entity\MaMaquina;
+use DG\AdminBundle\Form\MaMaquinaType;
+use Symfony\Component\HttpKernel\Exception;
+
+/**
+ * Maquina controller.
+ *
+ * @Route("admin/maquina")
+ */
+class MaquinaController extends Controller
+{
+    /**
+     * Lists all MaMaquina entities.
+     *
+     * @Route("/", name="admin_maquina_index",options={"expose"=true})
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $maquina = $em->getRepository('DGAdminBundle:MaMaquina')->findAll();
+
+        return $this->render('maquinaria/index.html.twig', array(
+            'maquina' => $maquina,
+        ));
+    }
+
+    /**
+     * Lists all MaMaquina entities.
+     *
+     * @Route("/nuevamaquina", name="nuevamaquina")
+     * @Method("GET")
+     */
+    public function NuevaMaquinaAction()
+    {
+        
+        return $this->render('maquinaria/nuevo.html.twig', array(
+            
+        ));
+    }
+    
+    
+    /**
+    * Ajax utilizado para buscar informacion de abogados
+    * 
+    * @Route("/buscarTipoEquipo", name="buscarTipoEquipo",options={"expose"=true})
+    */
+    public function BuscarTipoEquipoAction(Request $request)
+    {
+        $busqueda = $request->query->get('q');
+        $page = $request->query->get('page');
+       
+        $em = $this->getDoctrine()->getEntityManager();
+        $dql = "SELECT abo.id abogadoid, abo.nombre  "
+                        . "FROM DGAdminBundle:MaTipoEquipo abo "
+                        . "WHERE upper(abo.nombre) LIKE upper(:busqueda)"
+                        . " AND abo.estado=1 "
+                        . "ORDER BY abo.nombre ASC ";
+       
+        $abogado['data'] = $em->createQuery($dql)
+                ->setParameters(array('busqueda'=>"%".$busqueda."%"))
+                ->setMaxResults( 10 )
+                ->getResult();
+       
+        return new Response(json_encode($abogado));
+    }
+
+    
+   /**
+     * 
+     *
+     * @Route("/cliente/data", name="cliente_data")
+     */
+    public function DataClientePotencialAction(Request $request)
+    {
+        
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	 * Easy set variables
+	 */
+	
+	/* Array of database columns which should be read and sent back to DataTables. Use a space where
+	 * you want to insert a non-database field (for example a counter or static image)
+	 */
+        $entity = new MaMaquina();
+        $start = $request->query->get('start');
+        $draw = $request->query->get('draw');
+        $longitud = $request->query->get('length');
+        $busqueda = $request->query->get('search');
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $territoriosTotal = $em->getRepository('DGAdminBundle:Cliente')->findAll();
+        $territorio['draw']=$draw++;  
+        $territorio['recordsTotal'] = count($territoriosTotal);
+        $territorio['recordsFiltered']= count($territoriosTotal);
+        
+        $territorio['data']= array();
+        //var_dump($busqueda);
+        //die();
+        $arrayFiltro = explode(' ',$busqueda['value']);
+        
+        
+        //echo count($arrayFiltro);
+        $busqueda['value'] = str_replace(' ', '%', $busqueda['value']);
+        if($busqueda['value']!=''){
+        
+                    $dql = "SELECT cp.id , cp.nombre as nombre,cp.telefono as telefono, cp.direccion as direccion, concat(concat('<input type=\"checkbox\" class=\"checkbox idCliente\" id=\"',cp.id), '\">' as link "
+                            . ", concat('<a class=\"btn btn-success CP\" id=\"',cp.id, '\">Ver mas</a>') as link2 FROM DGAdminBundle:Cliente cp  "
+                        . "WHERE upper(cp.nombre)  LIKE upper(:busqueda) AND cp.estado=1 "
+                        . "ORDER BY cp.nombre DESC ";
+                    
+                    //Aqui estas trabjando
+                   $territorio['data'] = $em->createQuery($dql)
+                            ->setParameters(array('busqueda'=>"%".$busqueda['value']."%"))
+                            ->getResult();
+                    
+                   $territorio['recordsFiltered']= count($territorio['data']);
+                    
+                    $dql = "SELECT cp.id , cp.nombre as nombre,cp.telefono as telefono, cp.direccion as direccion, concat(concat('<input type=\"checkbox\" class=\"checkbox idCliente\" id=\"',cp.id), '\">' as link"
+                            . ", concat('<a class=\"btn btn-success CP \" id=\"',cp.id, '\">Ver mas</a>') as link2 FROM DGAdminBundle:Cliente cp  "
+                        . "WHERE upper(cp.nombre)  LIKE upper(:busqueda)  AND cp.estado=1 "
+                        . "ORDER BY cp.nombre DESC ";
+                   
+                   $territorio['data'] = $em->createQuery($dql)
+                            ->setParameters(array('busqueda'=>"%".$busqueda['value']."%"))
+                            ->setFirstResult($start)
+                            ->setMaxResults($longitud)
+                            ->getResult();
+       
+        }
+        else{
+            $dql = "SELECT cp.id , cp.nombre as nombre,cp.telefono as telefono, cp.direccion as direccion, concat(concat('<input type=\"checkbox\" class=\"checkbox idCliente\" id=\"',cp.id), '\">' as link,"
+                    . " concat('<a class=\"btn btn-success CP\" id=\"',cp.id, '\">Ver mas</a>') as link2 FROM DGAdminBundle:Cliente cp  "
+                    . " WHERE  cp.estado=1"
+                    . " ORDER BY cp.nombre  DESC ";
+                    $territorio['data'] = $em->createQuery($dql)
+                    ->setFirstResult($start)
+                    ->setMaxResults($longitud)
+                    ->getResult();
+        }
+     
+        
+        return new Response(json_encode($territorio));
+    }
+    
+   
+    
+ 
+    
+    
+    
+    
+    
+    
+}
---
+++ web/Resources/js/maquina/maquina.js
@@ -0,0 +1,81 @@
+ $(document).ready(function(){
+     
+     
+$('#anhoMaquina').Zebra_DatePicker({
+    format: 'Y'
+});
+     
+    $("#colorMaquina").select2({
+         placeholder: 'Seleccione un color',
+        
+    });
+    
+     
+     
+     $('#tipoEquipo').select2({
+                ajax: {
+                    url: Routing.generate('buscarTipoEquipo'),
+                    dataType: 'json',
+                    delay: 250,
+                    data: function (params) {
+                        return {
+                            q: params.term,
+                            page: params.page
+                        };
+                    },
+                    processResults: function (data, params) {
+                        var select2Data = $.map(data.data, function (obj) {
+                            obj.id = obj.abogadoid;
+                            obj.text = obj.nombre;
+
+                            return obj;
+                        });
+
+                        return {
+                            results: select2Data
+                        };
+                    },
+                    cache: true
+                },
+                escapeMarkup: function (markup) { return markup; },
+                minimumInputLength: 1,
+                templateResult: formatRepo,
+                templateSelection: formatRepoSelection,
+                formatInputTooShort: function () {
+                    return "Ingrese un caracter para la busqueda";
+                }
+            });
+
+       $(document).on("click","#guargarDatosGeneralesMaquina",function() {
+           
+           
+           
+           
+           
+       }); 
+     
+     
+     
+
+ });
+ 
+ function formatRepo (data) {
+            if(data.nombre){
+                var markup = "<div class='select2-result-repository clearfix'>" +
+                             "<div class='select2-result-repository__meta'>" +
+                             "<div class='select2-result-repository__title'>" + data.nombre+ "</div>" +
+                             "</div></div>";
+            } else {
+                var markup = "Seleccione un tipo de equipo";
+            }
+
+            return markup;
+        }
+
+        function formatRepoSelection (data) {
+            if(data.nombre){
+                return  data.nombre;
+            } else {
+                return "Seleccione un tipo de equipo";
+            }   
+        }
